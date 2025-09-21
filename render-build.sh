f09230fcb8d039f901fa1172e8dddcae873c2e19
Vansh Singh <Vanshsinghx21@gmail.com>
2025-09-21 20:43:32 +0530

    Fix: install ODBC Driver 17 for SQL Server on Render build

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-# Update and install system dependencies for MSSQL
+# Update & install system dependencies
 apt-get update -y
-apt-get install -y freetds-dev freetds-bin unixodbc-dev
+apt-get install -y curl gnupg2 apt-transport-https
 
-# Upgrade pip and install Python dependencies
+# Add Microsoft repo for ODBC drivers
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# Install ODBC Driver 17 and dependencies
+apt-get update -y
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+
+# Upgrade pip and install Python deps
 pip install --upgrade pip --no-cache-dir
 pip install pyodbc --no-cache-dir
 pip install -r requirements.txt --no-cache-dir
